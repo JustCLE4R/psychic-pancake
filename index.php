301cee421e46e6c04c47f94f624d4c2fe634acf7
Dimas Yudistira <alex.yudistira44@gmail.com>
2023-07-01 00:18:32 +0700

    merapikan kode (lagi)

--- index.php
+++ index.php
@@ -28,12 +28,7 @@ if (isset($_POST['sapmit'])) {
         <label for="username">username</label>
         <input type="text" name="username" id="username" required>
         <button name="sapmit">Submit</button>
-        <p class="test 1 2 3">Lorem ipsum dolor sit amet consectetur adipisicing elit. At quis velit eligendi aut dicta perferendis ducimus, obcaecati maiores hic ut veritatis fugit excepturi cum quisquam blanditiis accusamus impedit iure natus?</p>
-        <form action="" method="post">
-            <label for="username">username</label>
-            <input type="text" name="username" id="username" required>
-            <button name="sapmit">Submit</button>
-        </form>
+    </form>
 </body>
 
 </html>
