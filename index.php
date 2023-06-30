ab3af731e10a5fab661f5237682f0e68914c72f5
Dimas Yudistira <84244126+JustCLE4R@users.noreply.github.com>
2023-07-01 00:02:35 +0700

    Update index.php (menambah style untuk P)

--- index.php
+++ index.php
@@ -5,6 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Test Git</title>
+    <style>
+        p{
+            color : red;
+            font-size: 20vh;
+        }
+    </style>
 </head>
 <body>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At quis velit eligendi aut dicta perferendis ducimus, obcaecati maiores hic ut veritatis fugit excepturi cum quisquam blanditiis accusamus impedit iure natus?</p>
