b865c9ad97543fe75d4e4c57ab529f4d64fdf6af
Dimas Yudistira <alex.yudistira44@gmail.com>
2023-08-01 23:46:38 +0700

    latihan pengkondisian / percabangan

---
+++ basic-javascript/Looping/script.js
@@ -0,0 +1,11 @@
+// var nilai = 0;
+
+// while(nilai <= 10){
+//     console.log('Hello World! ' + nilai + ' kali');
+//     nilai ++;
+// }
+
+// for(var i = 1; i<=10; i++){
+//     console.log(i);
+// }
+
---
+++ basic-javascript/latihan/angkot2.js
@@ -0,0 +1,13 @@
+var nomorAngkot = 1;
+var jumlahAngkot = 10;
+var angkotBeroperasi = 8;
+
+while(nomorAngkot <= angkotBeroperasi){
+    console.log('Angkot No. '+nomorAngkot+' beroperasi dengan baik.');
+    nomorAngkot++;
+}
+
+for(nomorAngkot;nomorAngkot<=jumlahAngkot;nomorAngkot++){
+    console.log('Angkot No. '+nomorAngkot+' sedang tidak beroperasi.');
+}
+
